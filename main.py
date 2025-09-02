a46dcf5b172793a7de3c212f74df47c94c6f589a
Peter Möller <peter.moller@student.nhlstenden.com>
2025-09-02 15:09:37 +0200

    made the frog appear on a different location each time to figure out how to move it around later

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 import tkinter
 import random
 
+number = random.randint(10, 1000)
+
 IMGPATH = './img/frog.png'
 WINDOW = tkinter.Tk()
+WINDOW.geometry('420x420+'+str(number)+'+100')
 
 def main():
     # set the image
     img = tkinter.PhotoImage(file=IMGPATH)
 
     # make a label
-    label = tkinter.Label(image=img)
+    label = tkinter.Label(image=img, bd=0)
     label.pack()
 
     # main loop
