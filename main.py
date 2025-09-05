66a666cf73ba612b9578170616f2ca33847fdcad
Peter Möller <peter.moller@student.nhlstenden.com>
2025-09-05 11:28:45 +0200

    added comments

--- main.py
+++ main.py
@@ -1,18 +1,31 @@
 import tkinter
 import random
 
-
+# image location
 IMGPATH = './img/frog.png'
+
+# initialize window
 WINDOW = tkinter.Tk()
+
+# make it so theres no background
 WINDOW.configure(bg='')
+
+# removes the program header
 WINDOW.overrideredirect(True)
+
+# make it so you can exit the program using the escape key
 WINDOW.focus_force()
 WINDOW.bind("<Escape>", lambda e: WINDOW.destroy())
 
 def move_around():
+    # set random x and y values
     x = random.randint(10, 1000)
     y = random.randint(10, 1000)
+
+    # change the location of the window
     WINDOW.geometry(f'226x223+{x}+{y}')
+
+    # call the function again after 50ms
     WINDOW.after(50, move_around)
 
 def main():
