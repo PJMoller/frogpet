f9307fa3aace79c7c91bcc62c2226a886a656a57
Peter Möller <peter.moller@student.nhlstenden.com>
2025-09-05 11:21:16 +0200

    made the frog move around

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 import tkinter
 import random
 
-number = random.randint(10, 1000)
+
 IMGPATH = './img/frog.png'
 WINDOW = tkinter.Tk()
-WINDOW.geometry('226x223+'+str(number)+'+100')
 WINDOW.configure(bg='')
 WINDOW.overrideredirect(True)
 WINDOW.bind("<Escape>", lambda e: WINDOW.destroy())
+
+def move_around():
+    x = random.randint(10, 1000)
+    y = random.randint(10, 1000)
+    WINDOW.geometry(f'226x223+{x}+{y}')
+    WINDOW.after(50, move_around)
 
 def main():
     # set the image
@@ -17,6 +22,8 @@ def main():
     label = tkinter.Label(image=img, bd=0)
     label.pack()
 
+    move_around()
+    
     # main loop
     WINDOW.mainloop()
 
