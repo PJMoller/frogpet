b8538472223b17e0561155c29dbe975468e358aa
Peter Möller <peter.moller@student.nhlstenden.com>
2025-09-16 13:57:49 +0200

    made it so the frog doesnt move around for now

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ IMGPATH = './img/frog.png'
 
 # initialize window
 WINDOW = tkinter.Tk()
-WINDOW_WIDTH = 226
-WINDOW_HEIGHT = 223
+WINDOW_WIDTH = 0
+WINDOW_HEIGHT = 0
 SCREEN_WIDTH = WINDOW.winfo_screenwidth()
 SCREEN_HEIGHT = WINDOW.winfo_screenheight()
 
@@ -21,44 +21,37 @@ WINDOW.focus_force()
 WINDOW.bind("<Escape>", lambda e: WINDOW.destroy())
 
 # set start location and acceleration
-START_POSITION_X = SCREEN_WIDTH - WINDOW_WIDTH - 50
-START_POSITION_Y = SCREEN_HEIGHT - WINDOW_HEIGHT - 50
-ACCELERATION_SPEED_X = 3
-ACCELERATION_SPEED_Y = 3
+START_POSITION_X = 0
+START_POSITION_Y = 0
 
+# Image initilization
 IMG = None
 
-
+# Start the program by setting all the values to what they should be
 def wake_up():
-    global IMG
+    global IMG, WINDOW_WIDTH, WINDOW_HEIGHT, START_POSITION_X, START_POSITION_Y
 
     # set the image
     IMG = tkinter.PhotoImage(file=IMGPATH)
+
+    # Set the width and height
+    WINDOW_WIDTH = IMG.width()
+    WINDOW_HEIGHT = IMG.height()
+
+    # Set the start position
+    START_POSITION_X = SCREEN_WIDTH - WINDOW_WIDTH - 50
+    START_POSITION_Y = SCREEN_HEIGHT - WINDOW_HEIGHT - 50
 
     # make a label
     label = tkinter.Label(image=IMG, bd=0)
     label.pack()
 
-def move_around():
-    global START_POSITION_X, START_POSITION_Y, ACCELERATION_SPEED_X, ACCELERATION_SPEED_Y
-
-    # update position
-    START_POSITION_X += ACCELERATION_SPEED_X
-    START_POSITION_Y += ACCELERATION_SPEED_Y
-
-    if START_POSITION_X <= 0 or START_POSITION_X + WINDOW_WIDTH >= SCREEN_WIDTH:
-        ACCELERATION_SPEED_X = -ACCELERATION_SPEED_X
-    if START_POSITION_Y <= 0 or START_POSITION_Y + WINDOW_HEIGHT >= SCREEN_HEIGHT:
-        ACCELERATION_SPEED_Y = -ACCELERATION_SPEED_Y
-
+    # Set the location
     WINDOW.geometry(f'{WINDOW_WIDTH}x{WINDOW_HEIGHT}+{START_POSITION_X}+{START_POSITION_Y}')
 
-    # call the function again after 50ms
-    WINDOW.after(50, move_around)
 
 def main():
     wake_up()
-    move_around()
 
     # main loop
     WINDOW.mainloop()
